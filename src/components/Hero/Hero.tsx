a6815d898af3db4f408e9ce69e38d63524cd9725
Brijesh Kumar <bk10895@gmail.com>
2024-07-15 18:18:20 +0530

    feat: redesign Hero component for improved UX
    - Change layout of Hero component from row to column for better readability
    - Add new Button component to enhance user interaction
    - Update text style and structure to better emphasize potential earnings

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 
+import { Button } from '../ui';
 import { HeroNavItems } from '../../../data';
 import Link from 'next/link';
 
@@ -9,7 +10,7 @@ export const Hero = () => {
   const [selected, setSelected] = useState('Refer');
 
   return (
-    <div className='flex mx-auto justify-center items-center w-full px-4 mt-8'>
+    <div className='flex flex-col mx-auto justify-center items-center w-full px-4 mt-8'>
       <div className='bg-primary-100 bg-opacity-11 rounded-[38px] py-2 px-4 w-full max-w-4xl'>
         <nav className='flex flex-row justify-around text-secondary-100 font-normal text-[16px] sm:text-[25px]'>
           {HeroNavItems.map(item => (
@@ -34,9 +35,24 @@ export const Hero = () => {
         </nav>
       </div>
       <div>
-        <div>
-          <p>Let’s Learn & Earn</p>
+        <div className='flex flex-col mt-5 '>
+          <div className='flex flex-col text-secondary-100 font-bold text-[88px] leading-[88px]'>
+            <p>Let’s Learn</p>
+            <p>& Earn</p>
+          </div>
+          <div className='text-secondary-100 font-normal text-[40px] leading-[65px]'>
+            <p>Get a chance to win </p>
+            <p>
+              up-to{' '}
+              <span className='text-primary-100 font-bold text-[54px]  '>
+                Rs. 15,000
+              </span>
+            </p>
+          </div>
         </div>
+        <Button classNames='text-secondary-200 bg-primary-100 flex items-center justify-center text-xl font-normal px-[52px] '>
+          Refer Now
+        </Button>
       </div>
     </div>
   );
